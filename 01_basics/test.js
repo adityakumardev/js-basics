aaf968759c7fc7fc4c1ce901fbfcceb351f28876
Aditya <157486567+githubuser0702@users.noreply.github.com>
2024-02-12 09:33:29 +0000

    push: update notes

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,17 @@
-console.log("Aditya");
+// lets check what happens if we re-assign values to variables declared with different keywords (var, let & const)
+
+var name = "Aditya"
+let age = 21
+const gender = "male"
+email = "adityak.kumar78@gmail.com" // not explicitly declared the variable 
+                                    //( automatically declared as a window object in non-strict mode)
+let phoneNo // undefined!
+
+//reassign!
+
+name = "aditya kumar" // can be redeclared (var)
+age = "22" // can be redeclared (let)
+// gender = null // assignment to const variable (TypeError)
+email = "test@telegmail.com" // can be redeclared (window.object)
+
+console.table([name, age, gender, email, phoneNo])
