5954c4d06cebea726e677b9f8a8986cf97e6b613
Aditya <157486567+adityakumardev@users.noreply.github.com>
2024-02-15 17:01:43 +0000

    update: diffs in js

---
+++ 01_basics/conditionals.js
@@ -0,0 +1,3 @@
+console.log("2" == 2) // (true) // == only checks for matching values without considering the datatype, conversion exists if both are different in type
+
+console.log("2" === 2) // (false) // condition will be true only if both its types and values are same
