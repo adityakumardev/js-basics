28bec5b9472e4cf78bc3869cdcb9999ce76250f0
Aditya <157486567+adityakumardev@users.noreply.github.com>
2024-02-28 07:39:35 +0000

    scope levels

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,22 @@
+function one() {
+    const username = "Aditya"
+
+    function two() {
+        const website = "github"
+        console.log(username)
+    }
+    //console.log(website) // refError: website not defined (accessing variable out of scope)
+
+    two()
+}
+// one()
+
+if(true) {
+    const username = 'Aditya'
+    if(username) {
+        const website = " github"
+        console.log(username + website)
+    }
+    console.log(website) //refError
+}
+console.log(username); //refError
