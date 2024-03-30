03afb3266fe723a9d7c8c910b90b385a1d252f6a
Aditya <157486567+adityakumardev@users.noreply.github.com>
2024-03-30 07:58:57 +0000

    execution context

---
+++ 03_basics/05_execute.js
@@ -0,0 +1,20 @@
+// execution context & call stack
+
+// [{}] -> Global Execution Context (this)
+// value of .this in browser is window object
+// Javascript is a single threaded language which works on a process
+
+// Functional Execution Context
+// Eval Execution Context
+
+// [{}] -> memory creation phase (memory allocation of variables and objects)
+// => execution phase (execution of code line by line)
+
+let val1 = 10
+let val2 = 5
+function addNum(num1, num2) {
+    return total
+}
+let result1 = addNum(val1, val2)
+let result2 = addNum(10, 2)
+
